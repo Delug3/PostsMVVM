6db2a048d6599585171ed229f98607bbac1f20a7
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2021-04-07 10:53:50 +0200

    updated context

--- app/src/main/java/com/delug3/postsmvvm/persistency/PostsRoomDb.kt
+++ app/src/main/java/com/delug3/postsmvvm/persistency/PostsRoomDb.kt
@@ -1,11 +1,11 @@
-package com.delug3.healios.persistency
+package com.delug3.postsmvvm.persistency
 
-import android.content.Context
+import android.app.Application
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.delug3.healios.persistence.entity.PostsRoom
-import com.delug3.healios.persistency.dao.PostsDao
+import com.delug3.postsmvvm.persistence.entity.PostsRoom
+import com.delug3.postsmvvm.persistency.dao.PostsDao
 import kotlinx.coroutines.CoroutineScope
 
 @Database(entities = [PostsRoom::class], version = 1)
@@ -19,7 +19,7 @@ abstract class PostsRoomDb : RoomDatabase() {
         private var INSTANCE: PostsRoomDb? = null
 
         fun getDb(
-            context: Context,
+            context: Application,
             scope: CoroutineScope
         ): PostsRoomDb {
             return INSTANCE ?: synchronized(this) {
