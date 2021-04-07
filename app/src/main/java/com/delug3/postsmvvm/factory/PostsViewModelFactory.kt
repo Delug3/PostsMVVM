fa5f5dec2651749f6b7ed4e4b1f9e9d86cd7f926
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2021-04-07 10:44:14 +0200

    added and implemented Viewmodel Factory

---
+++ app/src/main/java/com/delug3/postsmvvm/factory/PostsViewModelFactory.kt
@@ -0,0 +1,13 @@
+package com.delug3.postsmvvm.factory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.delug3.postsmvvm.postslistdetails.PostsDetailsViewModel
+
+
+class PostsViewModelFactory(private val mParam: Int) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return PostsDetailsViewModel(mParam) as T
+    }
+}
