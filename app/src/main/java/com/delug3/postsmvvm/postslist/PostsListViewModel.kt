5420e2eafe1652ee37b7bc4c40e1e1e19d3f253e
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2021-04-07 10:52:35 +0200

    implemented getPosts method

--- app/src/main/java/com/delug3/postsmvvm/postslist/PostsListViewModel.kt
+++ app/src/main/java/com/delug3/postsmvvm/postslist/PostsListViewModel.kt
@@ -1,39 +1,27 @@
-package com.delug3.healios.postslist
+package com.delug3.postsmvvm.postslist
 
 
-import android.app.Application
 import android.util.Log
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
-import com.delug3.healios.model.Posts
-import com.delug3.healios.network.ApiClient
-import com.delug3.healios.network.ApiInterface
-import com.delug3.healios.persistency.PostsRoomDb
-import com.delug3.healios.persistence.entity.PostsRoom
-import com.delug3.healios.persistency.dao.PostsDao
+import androidx.lifecycle.ViewModel
+import com.delug3.postsmvvm.model.Posts
+import com.delug3.postsmvvm.network.ApiClient
+import com.delug3.postsmvvm.network.ApiInterface
+import com.delug3.postsmvvm.persistence.entity.PostsRoom
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 
-class PostsListViewModel(application: Application) : AndroidViewModel(application) {
+class PostsListViewModel: ViewModel()  {
     private val TAG = "POSTS"
-    private var postsDao: PostsDao = PostsRoomDb.getDb(application, viewModelScope).postsDao()
+    //private var postsDao: PostsDao = PostsRoomDb.getDb(this, viewModelScope).postsDao()
 
     private var postsList: MutableLiveData<List<Posts>>? = null
     private var mappedRoomList: List<PostsRoom>? = null
 
-    fun getPosts(): MutableLiveData<List<Posts>>? {
-        if (postsList == null) {
-            postsList = MutableLiveData<List<Posts>>()
-            getDataFromUrl()
-        }
-        return postsList
-    }
 
-
-    private fun getDataFromUrl() {
+    private fun getPostsFromUrl() {
 
         val service = ApiClient.client?.create(ApiInterface::class.java)
         val call = service?.posts
@@ -41,15 +29,14 @@ class PostsListViewModel(application: Application) : AndroidViewModel(applicatio
         call!!.enqueue(object : Callback<List<Posts?>?> {
             override fun onResponse(call: Call<List<Posts?>?>, response: Response<List<Posts?>?>) {
                 if (response.isSuccessful) {
-                    val result = response.body()
+                    //val result = response.body()
 
-                    postsList?.setValue(result as List<Posts>?);
-
+                    postsList?.value = response.body() as List<Posts>?
                     //improve this line
-                    mappedRoomList = result?.map {
+                    mappedRoomList = response.body()?.map {
                         it?.let { it1 ->
                             PostsRoom(
-                                it1.userID,
+                                it1.userId,
                                 it.id,
                                 it.title,
                                 it.body
@@ -70,4 +57,15 @@ class PostsListViewModel(application: Application) : AndroidViewModel(applicatio
         })
     }
 
+    fun getPosts(): MutableLiveData<List<Posts>>? {
+        if (postsList == null) {
+            postsList = MutableLiveData<List<Posts>>()
+            getPostsFromUrl()
+        }
+        return postsList
+    }
+
+    private var _currentPost = "Post Test"
+    val currentPost: String
+        get() = _currentPost
 }
