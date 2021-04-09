7feab5509535e4cfe630d1e08045d39e0a35b86d
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2021-04-09 12:12:54 +0200

    replaced gson usage with moshi

---
+++ app/src/main/java/com/delug3/postsmvvm/network/PostsApiService.kt
@@ -0,0 +1,45 @@
+package com.delug3.postsmvvm.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+
+private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+/**
+ * Retrofit object with the Moshi converter.
+ */
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+/**
+ * A public Api object that exposes the lazy-initialized Retrofit service
+ */
+object PostsApi {
+    val retrofitService: PostApiInterface by lazy { retrofit.create(PostApiInterface::class.java) }
+}
+
+
+//private var retrofit: Retrofit? = null
+/*
+@JvmStatic
+val client: Retrofit?
+    get() {
+        if (retrofit == null) {
+            retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+        return retrofit
+    }
+
+ */
