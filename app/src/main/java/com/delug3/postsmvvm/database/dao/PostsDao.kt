85ad771f6562b10371e31b565cebdb80ad0ff327
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2021-04-16 11:14:17 +0200

    updated and implemented Dao Insert and Query

--- app/src/main/java/com/delug3/postsmvvm/database/dao/PostsDao.kt
+++ app/src/main/java/com/delug3/postsmvvm/database/dao/PostsDao.kt
@@ -1,20 +1,23 @@
-package com.delug3.postsmvvm.persistency.dao
+package com.delug3.postsmvvm.database.dao
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.delug3.postsmvvm.persistence.entity.PostsRoom
+import com.delug3.postsmvvm.model.Posts
 
 @Dao
 interface PostsDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertAllPosts(postsRoomList: List<PostsRoom?>?);
+    suspend fun insertPosts(posts: Posts)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllPosts(posts: List<Posts?>?)
 
     @Query("SELECT * FROM posts_table")
-    fun readAllPosts(): LiveData<List<PostsRoom>>
+    fun readAllPosts(): LiveData<List<Posts>>
 
 
 }
