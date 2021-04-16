e9b7e245b7e660240bc70d17b00326e7e674cc13
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2021-04-16 11:12:53 +0200

    annotated data class as Room entity, added primary key

--- app/src/main/java/com/delug3/postsmvvm/model/Posts.kt
+++ app/src/main/java/com/delug3/postsmvvm/model/Posts.kt
@@ -1,8 +1,15 @@
 package com.delug3.postsmvvm.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "posts_table")
 data class Posts(
 
     val userId: Int,
+    @PrimaryKey
+    @ColumnInfo(name = "id")
     val id: Int,
     val title: String,
     val body: String
